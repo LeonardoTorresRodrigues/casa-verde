9ec256265e130de354eba27d881f8c890ed5587d
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-06-15 13:46:32 -0300

    refactor(home): new style to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
+import 'normalize.css';
+import Header from '@/components/Header';
 import React from 'react';
+import styled from 'styled-components';
+
+const HomePage = styled.div`
+	width: 100vw;
+	height: 100vh;
+	background-color: var(--background-white);
+`;
 
 export default function Home() {
 	return (
 		<>
-			<h1>Oi!</h1>
+			<HomePage>
+				<Header />
+				
+			</HomePage>
 		</>
 	);
 }
