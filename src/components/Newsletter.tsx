f7a5951611432374840fe8dacdebe75390c8f88c
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-06-26 12:31:03 -0300

    refactor(newsletter): new Newsletter div, add media queries

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -2,34 +2,63 @@ import React from 'react';
 import styled from 'styled-components';
 
 const NewsletterContainer = styled.div`
-	width: 36.563rem;
-	height: 28.875rem;
+	background-color: #ffe29a;
+	padding: 1rem;
+	margin-top: 1rem;
+	position: relative;
+	max-width: 36.5rem;
 
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-start;
+	@media(min-width: 920px) {
+		background-color: transparent;
+		margin-top: 9.6rem;
+	}
 
-	margin: 5rem 0;
+	:after {
+		content: "";
+		display: block;
+		width: 49rem;
+		height: 60.9rem;
+		position: absolute;
+		--baseDistance: -287px;
+		top: calc(var(--baseDistance) + 50px);
+		right: 0;
+		z-index: -1;
+		pointer-events: none;
+	}
+`;
+
+const NewsletterContainerText = styled.div`
+	max-width: 30rem;
+	margin-bottom: 2rem;
 `;
 
 const NewsletterTitle = styled.div`
-	p {
-		font-family: var(--ff-text);
-		font-weight: var(--fw-text);
-		font-size: 22px;
-		line-height: 27px;
-		color: var(--font-black);
-		opacity: 0.5;
-	}
+		h1 {
+			font-family: var(--ff-text);
+			font-weight: var(--fw-text);
+			font-style: normal;
+			font-size: 22px;
+			line-height: 27px;
+			letter-spacing: 0em;
+			text-align: left;
+			margin-bottom: 1.5rem;
+			color: var(--font-black);
+		}
+		h1 strong {
+			font-family: var(--ff-title);
+			font-size: 50px;
+			font-style: normal;
+			font-weight: var(--fw-title);
+			line-height: 1;
+			letter-spacing: 0em;
+			text-align: left;
+
+			@media(min-width: 920px) {
+				font-size: 82px;
+			}
+		}
+
 	
-	h1 {
-		font-family: var(--ff-title);
-		font-weight: var(--fw-title);
-		font-size: 82px;
-		line-height: 94px;
-		margin: 0;
-		color: var(--font-black);
-	}
 `;
 
 const NewsletterText = styled.div`
@@ -84,13 +113,19 @@ const NewsletterForm = styled.form`
 export default function Newsletter() {
 	return (
 		<NewsletterContainer>
-			<NewsletterTitle>
-				<p>Sua casa com as</p>
-				<h1>melhores plantas</h1>
-			</NewsletterTitle>
-			<NewsletterText>
-				<p>Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torna-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.</p>
-			</NewsletterText>
+			<NewsletterContainerText>
+				<NewsletterTitle>
+					<h1>Sua casa com as
+						<br />
+						<strong>
+							melhores plantas
+						</strong>
+					</h1>
+				</NewsletterTitle>
+				<NewsletterText>
+					<p>Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torna-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.</p>
+				</NewsletterText>
+			</NewsletterContainerText>
 			<NewsletterForm>
 				<input type="email" placeholder='Insira seu e-mail' />
 				<button>Assinar newsletter</button>
