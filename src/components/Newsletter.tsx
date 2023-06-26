b8ac09a7d7f8379eaa114ed84272fc10bf7e62a1
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-06-26 13:18:55 -0300

    refactor(newsletterform): correcting form style

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -63,48 +63,75 @@ const NewsletterText = styled.div`
 	font-family: var(--ff-text);
 	font-weight: var(--fw-text);
 	font-size: 16px;
+	font-style: normal;
+	letter-spacing: 0em;
+	text-align: left;
 	line-height: 26px;
+	margin-bottom: 1rem;
 	color: var(--font-black);
 	opacity: 0.5;
 `;
 
-const NewsletterForm = styled.form`
-	width: 36.563rem;
-	height: 4.688rem;
-
+const NewsletterForm = styled.div`
 	display: flex;
-
-	box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.06);
+	flex-direction: column;
+	
+	
 	background-color: var(--background-white);
 
+	@media(min-width: 920px) {
+		flex-direction: row;
+	}
+
 	input {
-		width: 70%;
-		height: 100%;
+		padding: 1.6rem;
+		padding-left: 3.4rem;
+		flex: 1;
 
 		font-family: var(--ff-text);
 		font-weight: var(--fw-text);
 		font-size: 16px;
+		font-style: normal;
 		line-height: 20px;
-
-		color: var(--font-black);
-		opacity: 0.3;
 		
+		letter-spacing: 0em;
+		text-align: left;
+
+		outline-color: var(--font-yellow);
+		color: var(--font-black);
+		opacity: 0.;
+		
+		box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.06);
 		border: none;
-		justify-content: center;
+	}
+
+	input:hover {
+		box-shadow: 10px 10px 30px 0px #0000002e;
 	}
 
 	button {
-		width: 30%;
-		height: 100%;
+		width: 100%;
+		height: 4.6rem;
+
+		left: 751px;
+		top: 597px;
+
+		padding: 1.6rem;
+		
+		border: none;
 		
 		color: var(--font-white);
+		background-color: var(--background-yellow);
+		box-shadow: 10px 10px 30px 0px #ffcb474d;
+
 		font-family: var(--ff-text);
 		font-weight: var(--fw-text);
 		font-size: 16px;
 		line-height: 20px;
 
-		background-color: var(--background-yellow);
-		border: none;
+		@media(min-width: 920px) {
+			width: initial;
+		}
 	}
 `;
 
@@ -124,10 +151,12 @@ export default function Newsletter() {
 					<p>Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torna-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.</p>
 				</NewsletterText>
 			</NewsletterContainerText>
-			<NewsletterForm>
-				<input type="email" placeholder='Insira seu e-mail' />
-				<button>Assinar newsletter</button>
-			</NewsletterForm>
+			<form action="/">
+				<NewsletterForm>
+					<input type="email" placeholder='Insira seu e-mail' />
+					<button>Assinar newsletter</button>
+				</NewsletterForm>
+			</form>
 		</NewsletterContainer>
 	);
 }
