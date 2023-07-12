45149cd6d83ca52c9327e9de40c7c095080b2265
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-07-12 12:44:29 -0300

    refactor(main-card): styling main card content

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
 import image from '../../public/images/image 3.png';
+import imageList from '../../public/images/ellipse.png';
 
 const MainCardContainer = styled.section`
 	width: 995px;
 	height: 440px;
-	border: 1px red solid;
 	display: flex;
 	justify-content: space-around;
-	margin: auto;
+	margin: 10rem auto;
+`;
+
+const MainCardContent = styled.div`
+	display: flex;
+	flex-direction: column;
 `;
 
 const MainCardTitle = styled.div`
@@ -36,6 +41,22 @@ const MainCardTitle = styled.div`
 	}
 `;
 
+const MainCardText = styled.div`
+	ul {
+		padding: 0;
+	}
+
+	li {
+		font-family: var(--ff-text);
+		font-weight: var(--fw-text);
+		font-size: 22px;
+		line-height: 27px;
+		opacity: 50%;
+		padding: 1rem 0;
+		list-style: none;
+	}
+`;
+
 export default function MainCard() {
 	return (
 		<MainCardContainer>
@@ -45,19 +66,47 @@ export default function MainCard() {
 				height={440}
 				alt='Folhas da uma planta'
 			/>
-			<MainCardTitle>
-				<h2>Como conseguir
-					<br />
-					<strong>
-						minha planta
-					</strong>
-				</h2>
-				<ul>
-					<li>Escolha sua plantas</li>
-					<li>Faça seu pedido</li>
-					<li>Aguarde na sua casa</li>
-				</ul>
-			</MainCardTitle>
-		</MainCardContainer>
+			<MainCardContent>
+				<MainCardTitle>
+					<h2>Como conseguir
+						<br />
+						<strong>
+							minha planta
+						</strong>
+					</h2>
+				</MainCardTitle>
+				<MainCardText>
+					<ul>
+						<li>
+							<Image
+								src={imageList}
+								width={52}
+								height={52}
+								alt='elipse'
+							/>
+							Escolha sua plantas
+						</li>
+						<li>
+							<Image
+								src={imageList}
+								width={52}
+								height={52}
+								alt='elipse'
+							/>
+							Faça seu pedido
+						</li>
+						<li>
+							<Image
+								src={imageList}
+								width={52}
+								height={52}
+								alt='elipse'
+							/>
+							Aguarde na sua casa
+						</li>
+					</ul>
+				</MainCardText>
+			</MainCardContent>
+		</MainCardContainer >
 	);
 }
